4eedf3c6a2568af04e139a3a1e84bf34d1541ace
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-10-09 16:45:09 +0300

    UI beautify & Encapsulation

--- lib/homeScreen/menu_select_button.dart
+++ lib/homeScreen/menu_select_button.dart
@@ -2,26 +2,34 @@ import 'package:flutter/material.dart';
 import '../crossAppUtilities/constants.dart';
 
 class MenuSelectionButton extends StatelessWidget {
-
   final String buttonText;
+  final IconData providedIcon;
+  final Color iconColor;
+  final Color cardColor;
 
-  MenuSelectionButton({this.buttonText = 'Please Provide a Text'});
+  MenuSelectionButton({required this.buttonText, required this.providedIcon, required this.iconColor, required this.cardColor});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: [
-        Container(
-          padding: EdgeInsets.all(kMenuButtonsSpacing),
-          child: Text(
-            buttonText,
-            style: TextStyle(
-              fontSize: kMenuButtonsTextSize,
-            ),
-          ),
+    return Card(
+      child: Container(
+        decoration: BoxDecoration(
+          color: cardColor,
+          border: Border.all(color: Colors.black45),
+          borderRadius: BorderRadius.circular(8.0),
         ),
-      ],
+        padding: EdgeInsets.symmetric(vertical: 14),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              buttonText,
+              style: TextStyle(fontSize: kMenuButtonsTextSize),
+            ),
+            Icon(providedIcon, size: 30, color: iconColor,),
+          ],
+        ),
+      ),
     );
   }
 }
