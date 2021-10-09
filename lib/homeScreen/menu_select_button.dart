75affe51ad6293621c4d7a1aadff1bdc41b80831
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-10-09 18:26:15 +0300

    complete navigation functionality

--- lib/homeScreen/menu_select_button.dart
+++ lib/homeScreen/menu_select_button.dart
@@ -6,28 +6,39 @@ class MenuSelectionButton extends StatelessWidget {
   final IconData providedIcon;
   final Color iconColor;
   final Color cardColor;
+  final String pageID;
 
-  MenuSelectionButton({required this.buttonText, required this.providedIcon, required this.iconColor, required this.cardColor});
+  MenuSelectionButton({
+    required this.buttonText, 
+    required this.providedIcon, 
+    required this.iconColor, 
+    required this.cardColor, 
+    required this.pageID});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Container(
-        decoration: BoxDecoration(
-          color: cardColor,
-          border: Border.all(color: Colors.black45),
-          borderRadius: BorderRadius.circular(8.0),
-        ),
-        padding: EdgeInsets.symmetric(vertical: 14),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              buttonText,
-              style: TextStyle(fontSize: kMenuButtonsTextSize),
-            ),
-            Icon(providedIcon, size: 30, color: iconColor,),
-          ],
+      child: GestureDetector(
+        onTap: () {
+          Navigator.pushNamed(context, pageID);
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            color: cardColor,
+            border: Border.all(color: Colors.black45),
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          padding: EdgeInsets.symmetric(vertical: 14),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                buttonText,
+                style: TextStyle(fontSize: kMenuButtonsTextSize),
+              ),
+              Icon(providedIcon, size: 30, color: iconColor,),
+            ],
+          ),
         ),
       ),
     );
