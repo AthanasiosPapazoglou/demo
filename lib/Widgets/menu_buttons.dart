4340cdf15fd3c09c0b3eb01d2683344fc36456f5
Athanasios Papapzoglou <athanasiospap95@gmail.com>
2021-10-19 22:49:28 +0300

    fdhg

--- lib/Widgets/menu_buttons.dart
+++ lib/Widgets/menu_buttons.dart
@@ -1,44 +1,73 @@
-import 'package:demo/providers/information.dart';
+import 'package:demo/providers/Information.dart';
 import 'package:demo/screens/recipes_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-List<Widget> menuButtons(BuildContext context) {
-  final menusData = Provider.of<Information>(context);
-  final menuPaths = menusData.menusPathsList;
-  final menusName = menusData.menusList;
-  List<Widget> buttons = [];
+class MenuButtonsColumn extends StatefulWidget {
+  const MenuButtonsColumn({Key? key}) : super(key: key);
 
-  for (int i = 0; i < menusName.length; i++) {
-    buttons.add(Container(
-      padding: const EdgeInsets.all(5),
-      decoration: const BoxDecoration(
-        border: Border.symmetric(
-          horizontal: BorderSide(color: Colors.black, width: 0.5),
-        ),
-      ),
-      child: TextButton(
-        onPressed: () async {
-          await menusData.setItemsMap(menuPaths[i]);
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => RecipesList(
-                menuNum: (i + 1),
-                menuPath: menuPaths[i],
-              ),
+  @override
+  _MenuButtonsColumnState createState() => _MenuButtonsColumnState();
+}
+
+class _MenuButtonsColumnState extends State<MenuButtonsColumn> {
+  bool _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return _isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: menuButtons(context) 
+            );
+  }
+
+  List<Widget> menuButtons(BuildContext context) {
+    final menusData = Provider.of<Information>(context);
+    final menuPaths = menusData.menusPathsList;
+    final menusName = menusData.menusList;
+
+    List<Widget> buttons = [];
+
+    for (int i = 0; i < menusName.length; i++) {
+      buttons.add(Container(
+        padding: const EdgeInsets.all(5),
+        decoration: const BoxDecoration(
+            border: Border.symmetric(
+                horizontal: BorderSide(color: Colors.black, width: 0.5))),
+        child: TextButton(
+          onPressed: () async {
+            setState(() {
+              _isLoading = true;
+            });
+            await menusData
+                .setItemsMap(menuPaths[i])
+                .then((_) {})
+                .then(
+                  (_) => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RecipesList(
+                        menuNum: (i + 1),
+                        menuPath: menuPaths[i],
+                      ),
+                    ),
+                  ),
+                )
+                .then((value) => setState(() {
+                      _isLoading = false;
+                    }));
+          },
+          child: SizedBox(
+            width: double.infinity,
+            child: Text(
+              menusName[i],
+              style: const TextStyle(color: Colors.black),
             ),
-          );
-        },
-        child: SizedBox(
-          width: double.infinity,
-          child: Text(
-            menusName[i],
-            style: const TextStyle(color: Colors.black),
           ),
         ),
-      ),
-    ));
+      ));
+    }
+    return buttons;
   }
-  return buttons;
 }
